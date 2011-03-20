e0af09fd8ba688ed8b15d7841417290e140bde64
adamayres <magicaj@gmail.com>
2011-03-19 19:08:57 -0700

    Fixed URL in bookmarklet.

--- war/js/stack-fiddle-bookmarklet.js
+++ war/js/stack-fiddle-bookmarklet.js
@@ -3,5 +3,5 @@ javascript: (function(d) {
     loadingElement.id = "sf-loader";
     loadingElement.setAttribute("style", "position:absolute; top:0px; width:100%;");
     loadingElement.innerHTML = "<div style='display:inline-block; font-weight:bold; padding:5px; background:#FFF1A8;'>Loading Stack Fiddle Libs...</div>";
-    d.body.appendChild(d.createElement("script")).src = "http:stackfiddle.com/js/stack-fiddle.min.js";
+    d.body.appendChild(d.createElement("script")).src = "http://stackfiddle.com/js/stack-fiddle.min.js";
 })(document);
