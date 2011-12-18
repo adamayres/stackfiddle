5a240e5c7268b9654a2e7716ec8ffde2ef7b6b5e
adamayres <magicaj@gmail.com>
2011-12-17 21:05:47 -0800

    Undoing GAE structure, will find a new host.

---
+++ src/stack-fiddle-bookmarklet.js
@@ -0,0 +1,7 @@
+javascript: (function(d) {
+    var loadingElement = d.body.appendChild(d.createElement("div"));
+    loadingElement.id = 'sf-loader';
+    loadingElement.setAttribute("style", "position:fixed; left:50%; top:0px; width:100%;");
+    loadingElement.innerHTML = "<div style='position:relative; right:50%; font-weight:bold; padding:5px; background:#FFF1A8;'><div>Loading Stack Fiddle Libs...</div></div>";
+    d.body.appendChild(d.createElement("script")).src = "http://stackfiddle.com/js/stack-fiddle.min.js";
+})(document);
--- war/js/stack-fiddle-bookmarklet.js
+++
@@ -1,7 +0,0 @@
-javascript: (function(d) {
-    var loadingElement = d.body.appendChild(d.createElement("div"));
-    loadingElement.id = 'sf-loader';
-    loadingElement.setAttribute("style", "position:absolute; top:0px; width:100%;");
-    loadingElement.innerHTML = "<div style='display:inline-block; font-weight:bold; padding:5px; background:#FFF1A8;'>Loading Stack Fiddle Libs...</div>";
-    d.body.appendChild(d.createElement("script")).src = "http://stackfiddle.com/js/stack-fiddle.min.js";
-})(document);
